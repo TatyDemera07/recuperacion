924fb73e6cd4eb3609aa684afa46884010c0addc
TatyDemera07 <102629946+TatyDemera07@users.noreply.github.com>
2023-02-11 08:46:52 -0500

    Recuperacion

---
+++ EXAMEN RECUPERACION/js/swetA.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Gracias!',
+    text: 'Esto es un poco de nosotros un gusto.',
+    imageUrl: 'https://www.recetasnestle.com.ec/sites/default/files/srh_recipes/4e4293857c03d819e4ae51de1e86d66a.jpg',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
